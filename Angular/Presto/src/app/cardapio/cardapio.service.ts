e54a0bfa330a548970034371c79b20b53c626a9c
Ezequiel-Ferreira <ezequielferre21@edu.unifor.br>
2020-10-03 19:10:33 -0300

    Com o filtro de produtos!

--- Angular/Presto/src/app/cardapio/cardapio.service.ts
+++ Angular/Presto/src/app/cardapio/cardapio.service.ts
@@ -1,3 +1,4 @@
+import { Cardapio } from './cardapio';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -14,12 +15,16 @@ export class CardapioService {
     return this.http.get<Produto[]>(`http://localhost:8080/produto/produtos`);
   }
 
+  cardapioPornome(nome : string){
+    return this.http.get<Cardapio>(`http://localhost:8080/cardapio/getnome/${nome}`);
+  }
+
   // produtoPorId(id: number): Observable<Produto> {
   //   return this.http.get<Produto>(`http://localhost:8080/produto/${id}`)
   // }
 
-  produtoPorTipo(tipo: string): Observable<Produto[]> {
-    return this.http.get<Produto[]>(`http://localhost:8080/produto/gettipo/${tipo}`);
+  produtoPorTipo(tipo: string, nome : string): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`http://localhost:8080/cardapio/getfiltro/${nome}/${tipo}`);
   }
 
 }
