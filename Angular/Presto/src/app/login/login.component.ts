eb69cc9738b7b3542e89b6b5c1d8b3740e87b530
Clailson Lucas <clailsonlucas@gmail.com>
2020-10-30 22:17:23 -0300

    Validador tela de login

--- Angular/Presto/src/app/login/login.component.ts
+++ Angular/Presto/src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from './../authService/authservice.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { element } from 'protractor';
+import { Router } from '@angular/router';
+import { Usuario } from '../usuario/usuario';
 
 
 
@@ -14,14 +16,17 @@ import { element } from 'protractor';
 })
 export class LoginComponent implements OnInit {
 
+
+  data: any;
+
+  usuario: Usuario;
+
   emailRedef = "";
 
   errorMessage = "";
-
   successMessage = "";
 
   errorMessageLogin = "";
-
   successMessageLogin = "";
 
   loading = false;
@@ -29,7 +34,7 @@ export class LoginComponent implements OnInit {
   signInForm : FormGroup;
   // return: string = '';
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private usuarioSevice : UsuarioService){ }
+  constructor(private fb: FormBuilder, private authService: AuthService, private usuarioSevice : UsuarioService, private route: Router){ }
 
   ngOnInit(): void {
     this.signInForm = this.fb.group(
@@ -43,14 +48,41 @@ export class LoginComponent implements OnInit {
   login() {
     console.log("Login chamado")
     console.log(this.signInForm.value.email)
-    this.authService.login(this.signInForm.value.email, this.signInForm.value.senha).subscribe();
+    this.authService.login(this.signInForm.value.email, this.signInForm.value.senha).subscribe(
+      // (response) => {
+      //   console.log('response')
+      //   this.successMessageLogin = response;
+      //   setTimeout(() => { this.route.navigate(['/pedidos']); }, 2000)
+      // },
+      // (error) => {
+      //   console.log('error')
+      //   this.errorMessageLogin = error
+      //   this.loading = false
+      // }
+      retorno => {
+        if (retorno) {
+          console.log('response')
+          this.successMessageLogin = "Login efetuado com sucesso";
+          setTimeout(() => { this.route.navigate(['/pedidos']); }, 2000)
+        } else {
+          console.log('error')
+          this.errorMessageLogin = "Usuario e senha invalido"
+          this.loading = false
+        }
+      },
+      (error) => {
+        console.log('error')
+        this.errorMessageLogin = error
+        this.loading = false
+      }
+    );
   }
 
   loggedUser() {
     this.authService.loggedUser()
   }
 
-async enviarSenhaPorEmail(){
+  async enviarSenhaPorEmail(){
     this.usuarioSevice.redefinirSenha(this.emailRedef).subscribe(
       (response)  => {
         console.log('response received');
